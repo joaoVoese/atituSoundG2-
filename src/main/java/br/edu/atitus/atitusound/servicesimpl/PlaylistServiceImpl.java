64f9c37d5039e080431d1332e2135a9c41c7793d
João Voese <voesecomercio@gmail.com>
2023-11-21 20:39:52 -0300

    .

--- src/main/java/br/edu/atitus/atitusound/servicesimpl/PlaylistServiceImpl.java
+++ src/main/java/br/edu/atitus/atitusound/servicesimpl/PlaylistServiceImpl.java
@@ -1,10 +1,14 @@
 package br.edu.atitus.atitusound.servicesimpl;
 
+import org.springframework.stereotype.Service;
+
 import br.edu.atitus.atitusound.entities.PlaylistEntity;
+
 import br.edu.atitus.atitusound.repositories.GenericRepository;
 import br.edu.atitus.atitusound.repositories.PlaylistRepository;
 import br.edu.atitus.atitusound.services.PlaylistService;
 
+@Service
 public class PlaylistServiceImpl implements PlaylistService {
 
 	private final PlaylistRepository playlistRepository;
