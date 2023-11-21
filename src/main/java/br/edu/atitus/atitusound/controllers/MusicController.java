0d00e1dcfad472e59b2cb05be805de949f66da49
João Voese <voesecomercio@gmail.com>
2023-11-21 19:35:58 -0300

    .

--- src/main/java/br/edu/atitus/atitusound/controllers/MusicController.java
+++ src/main/java/br/edu/atitus/atitusound/controllers/MusicController.java
@@ -1,11 +1,15 @@
 package br.edu.atitus.atitusound.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.edu.atitus.atitusound.dtos.MusicDTO;
 import br.edu.atitus.atitusound.entities.ArtistEntity;
 import br.edu.atitus.atitusound.entities.MusicEntity;
 import br.edu.atitus.atitusound.services.GenericService;
 import br.edu.atitus.atitusound.services.MusicService;
-
+@RestController
+@RequestMapping("/musics")
 public class MusicController extends GenericController<MusicEntity, MusicDTO>{
 
 	private final MusicService musicService;
